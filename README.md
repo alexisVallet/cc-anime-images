421f8250b5e736db7ab9018b411c5bca8121904c
Alexis Vallet <alexis.vallet@gmail.com>
2015-02-08 10:32:53 +0900

    Fixed kuroko license info, added readme and license.

---
+++ README.md
@@ -0,0 +1,3 @@
+Creative Commons licensed anime images
+-----
+This repository contains ~100 creative commons licensed images from deviantArt. Intended for use in my public facing blog articles, web applications, restrictive research journals. The information necessary for the licenses is stored in the info folder, while the images themselves are in the images folder. See IMAGES-LICENSE.md for full, image per image license information.
---
+++ generate_license.py
@@ -0,0 +1,35 @@
+""" Generates the IMAGES-LICENSES.md file from the images metadata.
+"""
+import os.path
+import os
+import json
+
+if __name__ == "__main__":
+    json_folder = 'info'
+    output_file = 'IMAGES-LICENSES.md'
+    json_fnames = [f for f in os.listdir(json_folder) 
+                   if f.endswith('.json')]
+    license_names = {
+        'cc-3.0': "[CC BY 3.0](http://creativecommons.org/licenses/by/3.0/)",
+        'cc-sa-3.0': "[CC BY-SA 3.0](http://creativecommons.org/licenses/by-sa/3.0/)",
+        'cc-nd-3.0': "[CC BY-ND 3.0](http://creativecommons.org/licenses/by-nd/3.0/)",
+        'cc-nc-3.0': "[CC BY-NC 3.0](http://creativecommons.org/licenses/by-nc/3.0/)",
+        'cc-nc-sa-3.0': "[CC BY-NC-SA 3.0](http://creativecommons.org/licenses/by-nc-sa/3.0/)",
+        'cc-nc-nd-3.0': "[CC BY-NC-ND 3.0](http://creativecommons.org/licenses/by-nc-nd/3.0/)"
+    }
+    # Delete the license content.
+    open(output_file, 'w').close()
+    with open(output_file, 'w') as output:
+        for json_fname in json_fnames:
+            print json_fname
+            with open(os.path.join(json_folder, json_fname)) as info_f:
+                info = json.load(info_f)
+                attribution = "'[{title}]({url})' by [{author}]({author}.deviantart.com) is licensed under {license}"
+                formatted = attribution.format(
+                    title=info['title'],
+                    author=info['author'],
+                    url=info['url'],
+                    license=license_names[info['license_type']]
+                )
+                output.write(formatted + '\n')
+
